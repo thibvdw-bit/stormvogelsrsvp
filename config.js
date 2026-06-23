d3b40d996b519e4215ea99f26e1e3e39c66de840
thibvdw-bit <thib.vdw@gmail.com>
2026-06-23 22:56:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Plak hier na de Google-Sheets-installatie de URL van je Apps Script-webapp.
 // Zonder URL werkt de site in lokale demomodus op één toestel.
 window.STORMVOGELS_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbw-Ujx8uOrgIr2GdsPTuqgBWXBo40Ei6OKw8WCTqXQ2/dev",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzIsBx08lpruIfd6IK7RmVkyiYwJFz44W_IbgUD0RS83PCyflxX5Q2zCWo6so--scldXg/exec",
 };
