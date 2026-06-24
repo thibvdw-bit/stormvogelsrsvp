6920e084ba6853993aaf10fdfb224c8d1ef73ee0
thibvdw-bit <thib.vdw@gmail.com>
2026-06-24 09:59:08 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Plak hier na de Google-Sheets-installatie de URL van je Apps Script-webapp.
-// Zonder URL werkt de site in lokale demomodus op één toestel.
-window.STORMVOGELS_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzIsBx08lpruIfd6IK7RmVkyiYwJFz44W_IbgUD0RS83PCyflxX5Q2zCWo6so--scldXg/exec",
-};
