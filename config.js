7a1cf37f61a13c9a59d9a0a22de52fe171a6df0e
thibvdw-bit <thib.vdw@gmail.com>
2026-06-24 11:46:42 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Plak hier na de Google-Sheets-installatie de URL van je Apps Script-webapp.
+// Zonder URL werkt de site in lokale demomodus op één toestel.
+window.STORMVOGELS_CONFIG = {
+  apiUrl: "https://script.google.com/macros/s/AKfycbyzlQGEGPvAqkbuLQhA4KIzfv5Zfcol1JS06qq7_oajVtfZLQGUGEe13oVxEtbuU5I/exec",
+};
